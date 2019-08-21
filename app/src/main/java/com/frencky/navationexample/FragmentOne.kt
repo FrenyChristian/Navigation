2c4cc071dee54313fca200815c41c7ac7d69d84c
frenychristian <frenymecwan72@gmail.com>
2019-08-21 23:55:33 +0530

    initial commit

---
+++ app/src/main/java/com/frencky/navationexample/FragmentOne.kt
@@ -0,0 +1,33 @@
+package com.frencky.navationexample
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_one.*
+
+
+class FragmentOne : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_one, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button.setOnClickListener {
+            val action =  FragmentOneDirections.fragmentOneToFragmentTwo()
+            action.setNameUser(editText.text.toString())
+            findNavController().navigate(action)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/frencky/navationexample/FragmentTwo.kt
@@ -0,0 +1,29 @@
+package com.frencky.navationexample
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_two.*
+
+
+class FragmentTwo : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_two, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val name = FragmentTwoArgs.fromBundle(arguments).nameUser
+        txtWelcome.text = "Hello $name!"
+
+    }
+
+}
